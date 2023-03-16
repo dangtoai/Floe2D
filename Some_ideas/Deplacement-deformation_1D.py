c64a4c1c5426068e82ec5f449392cd424d6404f9
dangtoai <dangtoai01@gmail.com>
2023-03-16 17:19:57 +0100

    add elastic energy studies, compression and energy comparaison. Define a criteria of caracteristic compression time. Studies of big number of node in 1 dimension collision's problem

--- Some_ideas/Deplacement-deformation_1D.py
+++ Some_ideas/Deplacement-deformation_1D.py
@@ -10,23 +10,23 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-# import matplotlib
-# from collections import deque
+import matplotlib
+from collections import deque
 
-N = 20       #nombre de noeuds 
-L = 1.      #longueur du reseau
-k = 10000.     #raideur
-m = 1.      #masse
-mu = 400.     #viscosity
+N = 50          #nombre de noeuds 
+L = 1.          #longueur du reseau
+k = 100.      #raideur de chaque ressort
+m = 1.          #masse
+mu = 40.       #viscosity
 x0 = np.array(np.linspace(0,L,N))       #position initiale des noeuds
 v0 = np.zeros(N-1)                  
-v  = np.array([-0.25*10])               #vitesse init noeud libre
+v  = np.array([-0.25*1])               #vitesse init noeud libre
 v0 = np.concatenate([v0, v])            #vitesse initiale des noeuds
 L0 = L/(N-1)*np.ones(N-1)               #longueurs a vides de chaque ressorts
-t_simu = 2.
-n  = 500
+t_simu = 3.
+n  = 1000
 dt = t_simu/n
-history_len = 500
+
 
 #on utilise la meme notation que Desmond dans son rapport
 
@@ -79,7 +79,7 @@ t,y  = Model(L, N, x0, v0)
 
 # plt.figure()
 # for i in range(N,2*N):
-#     plt.plot(t,y[:,i],label = i-N)
+#     plt.plot(t[:10],y[:,i][:10],label = i-N)
 # plt.xlabel("times")
 # plt.ylabel("velocities")   
 # plt.legend()
@@ -97,52 +97,100 @@ t,y  = Model(L, N, x0, v0)
 ##mesure la deformation. 
 
 # print(y[500,:N]) # la position courant des noeuds à l'instant 500 
+
+Compression = np.zeros_like(t)
+for i in range(n+1):
+    Compression[i] = y[i,:N][-1]-L
 plt.figure()
-for i in range(0,50,5):
+plt.plot(t,Compression, label = "Compression")
+plt.hlines(0, xmin = 0, xmax = t_simu, color='black' )
+plt.xlabel("time")
+plt.legend()
+
+plt.figure()
+for i in range(0,5):
     plt.plot(x0, y[i,:N], "--")
 plt.xlabel("x") 
 plt.ylabel("$\phi$- Deformation field")
 
 ## mesure le champs de deplacement 1D
+
+
+# plt.figure()
+# plt.plot(t,)
+
+### using energie to determine compression time
+### energie cinetique: 
 plt.figure()
-for i in range(0,50,5):
-    plt.plot(x0, y[i,:N]-y[0,:N], "--")
+E_c = np.zeros(n+1)
+E_l = np.zeros(n+1)
+for i in range(n+1):
+    E_c[i] = 0.5 * sum( y[:, N:][i]**2 )
+    E_l[i] = 0.5 * sum( (np.abs(y[i, :N][1:] - y[i, :N][0:-1]) - 1/(N-1))**2 )
+
+# plt.plot(t, E_c, label = 'Energie cinetique')
+
+M = np.where(E_l == max(E_l))[0][0]
+MM = np.where(Compression == min(Compression))[0][0]
+
+plt.plot(t[:], E_l[:], label = 'Energie elastique')
+plt.xlabel("time(s)") 
+plt.legend()
+
+plt.figure()
+for i in range(0,M):
+    plt.plot(x0, y[i,:N]-y[0,:N], "--",)
 plt.xlabel("x") 
+
+plt.figure()
+plt.plot(x0, y[M,:N]-y[0,:N], "--", label=" Max $E_l$ ")
+plt.plot(x0, y[MM,:N]-y[0,:N], "--", label = "Min Compression")
+
 plt.ylabel("$u=\phi-Id$- Displacement field")
+plt.legend()
 
+# plt.figure()
+# for i in range(0, M):
+#     plt.plot(x0, y[i,:N], "--")
+# plt.xlabel("x") 
+# plt.ylabel("$\phi$- Deformation field")
 
+# plt.figure()
+# for i in range(0, M):
+#     plt.plot(x0, y[i,:N]-y[0,:N], "--")
+# plt.xlabel("x") 
+# plt.ylabel("$u=\phi-Id$- Displacement field")
 
 #animation positions
-# fig = plt.figure(figsize=(5, 4))
-# ax = fig.add_subplot(autoscale_on=False, xlim=(-0.1, L+0.2), ylim=(-.2, .2))
-# ax.set_aspect('equal')
-# ax.grid()
-# line, = ax.plot([], [], 'o-', lw=2)
-# # trace, = ax.plot([], [], '.-', lw=1, ms=2)
-# time_template = 'time = %.1fs'
-# time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
-# history_x, history_y = deque(maxlen=history_len), deque(maxlen=history_len)
-# plt.title("Floe's evolution after choc")
+fig = plt.figure(figsize=(5, 4))
+ax = fig.add_subplot(autoscale_on=False, xlim=(-0.1, L+0.35), ylim=(-.2, .2))
+ax.set_aspect('equal')
+ax.grid()
+line, = ax.plot([], [], 'o-', lw=2)
+trace, = ax.plot([], [], '.-', lw=1, ms=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+history_x, history_y = deque(maxlen=n), deque(maxlen=n)
+plt.title("Floe's evolution after choc")
 
 
+def animate(i):
+    thisx = [y[:,j][i] for j in range(N)]
+    thisx.insert(0,0)
+    thisy = np.zeros_like(thisx)
+    if i == 0:
+        history_x.clear()
+        history_y.clear()
 
-# def animate(i):
-#     thisx = [y[:,j][i] for j in range(N)]
-#     thisx.insert(0,0)
-#     thisy = np.zeros_like(thisx)
-#     if i == 0:
-#         history_x.clear()
-#         history_y.clear()
+    history_x.appendleft(thisx[2])
+    history_y.appendleft(thisy[2])
 
-#     history_x.appendleft(thisx[2])
-#     history_y.appendleft(thisy[2])
+    line.set_data(thisx, thisy)
+    # trace.set_data(history_x, history_y)
+    time_text.set_text(time_template % (i*dt))
+    return line, time_text
 
-#     line.set_data(thisx, thisy)
-#     # trace.set_data(history_x, history_y)
-#     time_text.set_text(time_template % (i*dt))
-#     return line, time_text
-
-# ani = animation.FuncAnimation(fig, animate, len(y), interval=dt*1000, blit=True)
+ani = animation.FuncAnimation(fig, animate, len(y), interval=dt*1000, blit=True)
 # writergif = animation.PillowWriter(fps=30)
 # ani.save('filename.gif',writer=writergif)
 # plt.show()
