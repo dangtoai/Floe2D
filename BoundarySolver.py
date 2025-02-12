correct dynamical system of mass-spring network, computation of displacement field, extract Dirichlet condition

BoundarySolver.py
@@ -133,6 +133,11 @@ def P1_coefficient(Points, data):
 
 mesh = Mesh("file.msh")
 data = []
+
+
+
+# mesh.boundary_mesh.plot_dirichlet()
+# mesh.boundary_mesh.plot()
 with open('boundary_data.csv', 'r', encoding='utf-8') as csv_file:
     lines = csv_file.readlines()
     lines_to_read = lines[3:]
@@ -324,41 +329,41 @@ def f_y(x_eval, y_eval):
 def Dirichlet(x, y):
     return np.array([f_x(x, y), f_y(x, y)])
 
-grid_x, grid_y = np.meshgrid(np.linspace(min(xdata)-1, max(xdata)+1, num = 100),
-                            np.linspace(min(ydata)-1, max(ydata)+2, num = 100))
+# grid_x, grid_y = np.meshgrid(np.linspace(min(xdata)-1, max(xdata)+1, num = 50),
+                            # np.linspace(min(ydata)-1, max(ydata)+2, num = 50))
 
-grid_values_x = np.vectorize(f_x)(grid_x, grid_y)
-grid_values_y = np.vectorize(f_y)(grid_x, grid_y)
-# grid_values_f = np.vectorize(normfxy)(grid_x, grid_y)
+# grid_values_x = np.vectorize(f_x)(grid_x, grid_y)
+# grid_values_y = np.vectorize(f_y)(grid_x, grid_y)
+# # grid_values_f = np.vectorize(normfxy)(grid_x, grid_y)
 
-figax = plt.subplots()
-fig, ax = figax
-mesh.boundary_mesh.plot(figax)
-for i in range(data.shape[0]):
-    ax.plot(Points[i][0], Points[i][1], 'x')
-    # ax.text(Points[i][0], Points[i][1], str(i), color = 'red')
-contour_plot = ax.contourf(grid_x, grid_y, grid_values_x, cmap = 'viridis')
-ax.set_xlim(min(xdata)-1, max(xdata)+1)
-ax.set_ylim(min(ydata)-1, max(ydata)+2)
-cbar = plt.colorbar(contour_plot)
-cbar.set_label('Data Value')
+# figax = plt.subplots()
+# fig, ax = figax
+# mesh.boundary_mesh.plot(figax)
+# for i in range(data.shape[0]):
+#     ax.plot(Points[i][0], Points[i][1], 'x')
+#     # ax.text(Points[i][0], Points[i][1], str(i), color = 'red')
+# contour_plot = ax.contourf(grid_x, grid_y, grid_values_x, cmap = 'viridis')
+# ax.set_xlim(min(xdata)-1, max(xdata)+1)
+# ax.set_ylim(min(ydata)-1, max(ydata)+2)
+# cbar = plt.colorbar(contour_plot)
+# cbar.set_label('Data Value')
 
-figax = plt.subplots()
-fig, ax = figax
-mesh.boundary_mesh.plot(figax)
-for i in range(data.shape[0]):
-    ax.plot(Points[i][0], Points[i][1], 'x')
-    # ax.text(Points[i][0], Points[i][1], str(i), color = 'red')
-contour_plot = ax.contourf(grid_x, grid_y, grid_values_y, cmap = 'viridis')
-ax.set_xlim(min(xdata)-1, max(xdata)+1)
-ax.set_ylim(min(ydata)-1, max(ydata)+2)
-cbar = plt.colorbar(contour_plot)
-cbar.set_label('Data Value')
+# figax = plt.subplots()
+# fig, ax = figax
+# mesh.boundary_mesh.plot(figax)
+# for i in range(data.shape[0]):
+#     ax.plot(Points[i][0], Points[i][1], 'x')
+#     # ax.text(Points[i][0], Points[i][1], str(i), color = 'red')
+# contour_plot = ax.contourf(grid_x, grid_y, grid_values_y, cmap = 'viridis')
+# ax.set_xlim(min(xdata)-1, max(xdata)+1)
+# ax.set_ylim(min(ydata)-1, max(ydata)+2)
+# cbar = plt.colorbar(contour_plot)
+# cbar.set_label('Data Value')
 
-figax = plt.subplots()
-fig, ax = figax
-mesh.boundary_mesh.plot(figax)
-ax.quiver(grid_x, grid_y, grid_values_x, grid_values_y , scale = 1.)
+# figax = plt.subplots()
+# fig, ax = figax
+# mesh.boundary_mesh.plot(figax)
+# ax.quiver(grid_x, grid_y, grid_values_x, grid_values_y , scale = 1.)
 
 # figax = plt.subplots()
 # fig, ax = figax
@@ -372,56 +377,59 @@ ax.quiver(grid_x, grid_y, grid_values_x, grid_values_y , scale = 1.)
 # cbar = plt.colorbar(contour_plot)
 # cbar.set_label('Data Value')
 
-# logger = log.Log('griffith_solver.log', level=log.INFO, console_output=True)
-# logger.log_description(mesh_file=mesh,args=None)
-# log_queue = logger._log_queue
+logger = log.Log('griffith_solver.log', level=log.INFO, console_output=True)
+logger.log_description(mesh_file=mesh,args=None)
+log_queue = logger._log_queue
 
 T = problem_data.lame_tensor_ice #Lame tensor
 
-# # boundary_displacement = problem_data.Constant_Displacement_On_Y(abscissa_mid= 1005.)
+# boundary_displacement = problem_data.Constant_Displacement_On_Y(abscissa_mid= 1005.)
 boundary_displacement = problem_data.Boundary_Displacement_by_percussion(boundary_data = data, Dirichlet_func = Dirichlet)
-# # print(boundary_displacement.collision_point())
-physical_data = problem_data.Physical_Data(T, problem_data.Constant_Toughness(10.), boundary_displacement, initial_fracture=None)
+physical_data = problem_data.Physical_Data(T, problem_data.Constant_Toughness(1), boundary_displacement, initial_fracture=None)
 
 # print('start computation of classical energy')
 # classical_solution = solver.Classical_Solution(mesh=mesh, physical_data=physical_data)
-# # classical_solution.field.boundary_elements
-# # solution = solver.smart_time_solver(discretization_data, physical_data, log_queue, args.number_processes)
-# # Test = solver.Imposed_Fracture_Solution(mesh=mesh, physical_data=physical_data, fracture = None)
+# classical_solution.field.boundary_elements
+# solution = solver.smart_time_solver(discretization_data, physical_data, log_queue, args.number_processes)
+# Test = solver.Imposed_Fracture_Solution(mesh=mesh, physical_data=physical_data, fracture = None)
 
-# # print(classical_solution.energy)
+# print(classical_solution.energy)
 # classical_solution.plot_displacement()
 # classical_solution.plot_energy()
 
-# print('start computation of fractures')
-# boundary_point = [1011.299987792969, 104.0]
-# time_discretization = None
-# fracture_discretization = problem_data.Fracture_Discretization(angular_step = np.pi/5., boundary_point= boundary_point, lengh_step = 100 )
-# discretization_data =  problem_data.Discretization_Data(mesh, time_discretization, fracture_discretization, tip_enrichement=False)
 
-# find fracture from every boundary admissible points. 
+print('start computation of fractures')
+boundary_point = [1011.299987792969, 104.0]
+time_discretization = None
+fracture_discretization = problem_data.Fracture_Discretization(angular_step = np.pi/10., boundary_point= boundary_point, lengh_step = 100 )
+discretization_data =  problem_data.Discretization_Data(mesh, time_discretization, fracture_discretization, tip_enrichement=False)
+solution = solver.smart_time_solver(discretization_data, physical_data, log_queue)
+solution.plot_displacement()
+solution.plot()
+
+# # find fracture path from every boundary admissible points. 
 # f = fracture_iterators.Admissible_Boundary_Point(fracture_discretization, mesh.boundary_mesh)
+# f = fracture_iterators.Admissible_Fractures_From_Fixed_Boundary_Point(fracture_discretization, mesh, geo.Point(boundary_point[0], boundary_point[1]))
 # fig, ax = mesh.plot()
+
 # for pointsegment in f:
-#     point, segment = pointsegment
-#     boundary_point = [point.x, point.y]
-# # #     print(point)
-#     fracture_discretization = problem_data.Fracture_Discretization(angular_step = np.pi/5., boundary_point= boundary_point, lengh_step = 100)
-#     discretization_data =  problem_data.Discretization_Data(mesh, time_discretization, fracture_discretization, tip_enrichement=False)
-#     solution = solver.smart_time_solver(discretization_data, physical_data, log_queue)
-#     solution.plot()
-    # point.plot((fig, ax), marker='+', color='red')
-#     fracture_discretization = problem_data.Fracture_Discretization(angular_step = np.pi/20., boundary_point= [point.x, point.y], lengh_step = 100 )
-#     discretization_data =  problem_data.Discretization_Data(mesh, time_discretization, fracture_discretization, tip_enrichement=False)
-#     solution = solver.smart_time_solver(discretization_data, physical_data, log_queue)
-    # solution.plot_displacement()
+    # point, segment = pointsegment
+    # boundary_point = [point.x, point.y]
+    # print(pointsegment)
+    # fracture_discretization = problem_data.Fracture_Discretization(angular_step = np.pi/5., boundary_point= boundary_point, lengh_step = 1000)
+    # discretization_data =  problem_data.Discretization_Data(mesh, time_discretization, fracture_discretization, tip_enrichement=False)
+#     # solution = solver.smart_time_solver(discretization_data, physical_data, log_queue)
+#     # solution.plot()
+    # point.plot((fig, ax), marker='x', color='red')
+    # pointsegment.plot((fig, ax))
+#     # fracture_discretization = problem_data.Fracture_Discretization(angular_step = np.pi/20., boundary_point= [point.x, point.y], lengh_step = 100 )
+#     # discretization_data =  problem_data.Discretization_Data(mesh, time_discretization, fracture_discretization, tip_enrichement=False)
+#     # solution = solver.smart_time_solver(discretization_data, physical_data, log_queue)
+#     # solution.plot_displacement()
     # print(point, segment)
 
-# solution = solver.smart_time_solver(discretization_data, physical_data, log_queue)
-# solution.plot_displacement()
-# solution.plot()
+# f = fracture_iterators.Admissible_Fractures_From_Fixed_Boundary_Point(fracture_discretization, mesh, geo.Point(1011.299987792969, 104.0))
 
 
-# solution = solver.solver_with_time_discretization(discretization_data, physical_data, log_queue, number_processes = None)
-# solution.plot_displacement()
-# solution.plot_energy()
+
+

generate_geofile.py
@@ -10,18 +10,21 @@ import scipy as sp
 import csv
 import numpy as np
 from subprocess import run
-sys.path.append("/Users/phandangtoai/Documents/Floe2D/Griffith-master_Dimitri/")
+sys.path.append(
+    "/Users/phandangtoai/Documents/Floe2D/Griffith-master_Dimitri/")
 from griffith.geometry import Point, dist
+from griffith.mesh import Mesh
 
 Input = input("Select a geometry to run the random process: ")
 Input = int(Input)
 print(f"the geometry number {Input}")
-mat = sp.io.loadmat("Biblio_Floes.mat")['G'][Input][0] # 100 geometries of ice floes
+# 100 geometries of ice floes
+mat = sp.io.loadmat("Biblio_Floes.mat")['G'][Input][0]
 lc = 30.
 GEO_SCRIPT = "/* the geometry file\n */ \n\n"
 GEO_SCRIPT += f"lc = {lc}; // mesh precision \n"
 Points = []
-for i, point in enumerate(mat, start = 1):
+for i, point in enumerate(mat, start=1):
     GEO_SCRIPT += f"Point({i}) = {{{point[0]}, {point[1]}, 0 , lc}};\n"
     Points.append(Point(point[0], point[1]))
 
@@ -32,22 +35,39 @@ GEO_SCRIPT += f"Line({i+1}) = {{{i+1}, {1}}};\n"
 
 # line_loop_command = f"Line Loop(1) = {{{NUMBERS_STRING}}};"
 
-### define Dirichlet and Neumann part:
-with open('boundary_data.csv', mode = 'r') as csv_file:
+# define Dirichlet and Neumann part:
+with open('boundary_data.csv', mode='r') as csv_file:
     csvreader = csv.reader(csv_file)
     # print(csvreader)
     for row in csvreader:
         print(row)
 
-numbers = [float(num) for num in row[0].split()]
-contact_point = np.array(numbers)
+# numbers = [float(num) for num in row[0].split()]
+# contact_point = np.array(numbers)
+contact_point = np.array([1006.4074181139762, 119.04863615511762])
 
-THRESHOLD = 4.   ### TODO: Support of Dirichlet boundary depends on floe size
+# define 2 Dirichlet's region: 1 near the collision area and 1 far from it
+THRESHOLD = 4.  # TODO: Support of Dirichlet boundary depends on floe size
 
-index_dirichlet_points = np.array([dist( Point(contact_point[0], contact_point[1]), p) for p in Points]) < THRESHOLD
-index_dirichlet_points = np.where(index_dirichlet_points)[0]
-index_dirichlet_lines = range( min(index_dirichlet_points) - 1, max(index_dirichlet_points) + 1 )
-index_neumann_lines = [i for i in range(1,len(mat)) if i not in index_dirichlet_lines]
+# 1st Dirichlet's part
+Condition1 = np.array(
+    [dist(Point(contact_point[0], contact_point[1]), p) for p in Points]) < THRESHOLD
+index_dirichlet_points1 = np.where(Condition1)[0]
+print(index_dirichlet_points1)
+
+# 2nd Dirichlet's part
+Condition2 = np.array([dist(Point(contact_point[0], contact_point[1]), p)
+                      for p in Points]) > 7*THRESHOLD
+index_dirichlet_points2 = np.where(Condition2)[0]
+print(index_dirichlet_points2)
+
+combined_condition = Condition1 | Condition2
+index_dirichlet_lines = np.where(combined_condition)[0]
+print(index_dirichlet_lines)
+
+# index_dirichlet_lines = range( min(index_dirichlet_points1) - 1, max(index_dirichlet_points1) + 1 )
+index_neumann_lines = [i for i in range(
+    1, len(mat)) if i not in index_dirichlet_lines]
 
 NUMBERS_STRING = ', '.join(str(i) for i in index_neumann_lines)
 GEO_SCRIPT += f'Physical Line("N") = {{{NUMBERS_STRING}}};\n'
@@ -65,5 +85,7 @@ GEO_SCRIPT += 'Physical Surface("S") = {1};\n'
 with open('file.geo', 'w', encoding='utf-8') as geo_file:
     geo_file.write(GEO_SCRIPT)
 
-# run('gmsh -2 file.geo -o file.msh -format msh2', shell=True)
-# run('gmsh file.msh', shell = True)
+run('gmsh -2 file.geo -o file.msh -format msh2', shell=True)
+run('gmsh file.msh', shell = True)
+
+mesh = Mesh("file.msh")

generate_network.py
@@ -9,11 +9,13 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from scipy.spatial._qhull import Delaunay
+from scipy.spatial import Voronoi, voronoi_plot_2d
+
 from scipy import stats
 sys.path.append("/Users/phandangtoai/Documents/Floe2D/Griffith-master_Dimitri/")
 from griffith.geometry import Polygon, Point
 
-np.random.seed(11212325)
+np.random.seed(1212)
 
 
 ### set of points is in the counter clock sens
@@ -38,7 +40,7 @@ xDelta = xMax - xMin
 yDelta = yMax - yMin
 # print(xMin, xMax, yMin, yMax)
 
-LAMBDA = 0.1 # intensity of random process
+LAMBDA = 0.025 # intensity of random process
 num_points = int(LAMBDA * (poly.area()  )) # number of random points inside of the polygon
 
 # x coordinates of Poisson points
@@ -47,10 +49,16 @@ xx = xDelta * stats.uniform.rvs(0, 1, ((num_points, 1))) + xMin
 yy = yDelta * stats.uniform.rvs(0, 1, ((num_points, 1))) + yMin
 
 Points = []
+OutPoints = []
 for i in range(num_points):
+    # select interior point 
     if poly.has_point(Point(xx[i][0], yy[i][0])):
         Points.append( np.array([xx[i][0], yy[i][0]]))
+    else: OutPoints.append(np.array([xx[i][0], yy[i][0]]))
+    
 Points = np.array(Points)
+OutPoints = np.array(OutPoints)
+
 tri = Delaunay(Points)  # triangulation step
 Springs = set()
 for triangle in tri.simplices:
@@ -64,8 +72,21 @@ with open('masses-springs.csv', mode = 'w', newline = '', encoding='utf-8') as c
     csv_writer.writerows(Points)
     # csv_writer.writerows(Springs)
 
+# poly.plot()
+# plt.figure()
 poly.plot()
-plt.plot(Points[:,0], Points[:,1],'x', color = 'r')
+plt.plot(Points[:,0], Points[:,1],'o', color = 'orange')
+plt.plot(OutPoints[:,0], OutPoints[:,1], 'o', color = 'green')
+
+# fig, ax = plt.subplots()
+V = Voronoi(Points)
+fig, ax = poly.plot()
+voronoi_plot_2d(V, ax = ax)
+ax.set_xlim(xMin-1, xMax+1)
+ax.set_ylim(yMin-1, yMax+1)
+
+# plt.figure()
+poly.plot()
 plt.triplot(Points[:,0], Points[:,1], tri.simplices, color = 'b')
 plt.show()
 